75d40453dab38545e67d3227a3ef09ea93c48ef4
ravalrupalj <63676082+ravalrupalj@users.noreply.github.com>
2020-07-13 22:17:35 -0400

    Update

--- Selenium/Tutorial/Tutorial_40.py
+++ Selenium/Tutorial/Tutorial_40.py
@@ -1,8 +1,10 @@
-from selenium import webdriver
 import time
-driver= webdriver.Chrome(executable_path='C:\\Users\\raval\\Documents\\WebDrivers\\chromedriver_win32\\chromedriver.exe')
+from selenium import webdriver
+
+driver= webdriver.Chrome(executable_path='C:\\Users\\raval\\Documents\\chromedriver_win32\\chromedriver.exe')
 driver.get('https://login.salesforce.com/?locale=ca')
 driver.find_element_by_css_selector('#username').send_keys('Rupal')
 driver.find_element_by_css_selector('input.password').send_keys('Crazy')
 time.sleep(3)
 driver.find_element_by_css_selector('.password').clear()
+driver.find_element_by_link_text('Forgot Your Password?').click()
