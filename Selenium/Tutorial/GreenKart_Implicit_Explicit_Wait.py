02f115a73080dc7d4315199ac3df2d1236c0db1e
ravalrupalj <63676082+ravalrupalj@users.noreply.github.com>
2020-08-02 20:50:58 -0400

    Update

--- Selenium/Tutorial/GreenKart_Implicit_Explicit_Wait.py
+++ Selenium/Tutorial/GreenKart_Implicit_Explicit_Wait.py
@@ -11,21 +11,23 @@ from selenium.webdriver.support.wait import WebDriverWait
 driver= webdriver.Chrome(executable_path='C:\\Users\\raval\\Documents\\chromedriver_win32\\chromedriver.exe')
 driver.get('https://rahulshettyacademy.com/seleniumPractise/#/')
 driver.find_element_by_css_selector("[class='search-keyword']").send_keys('ber')
-time.sleep(3)
 #To check how many products gets diplayed if we write 'ber'
-grocery=driver.find_elements_by_xpath("//div[@class='products']/div")
+#go=driver.find_element_by_xpath("//div[@class='products']/div[1]/div[3]/button")
+wait=WebDriverWait(driver,10)
+#wait.until(expected_conditions.staleness_of(go))
+wait.until(expected_conditions.presence_of_all_elements_located((By.XPATH,"//div[@class='product']/div[3]/button")))
+grocery=driver.find_elements_by_xpath("//div[@class='product']/div[3]/button")
 #assert grocery==3
 for item in grocery:
-    add=item.find_element_by_css_selector("div[class='product-action'] button")
-    add.click()
+
+    item.click()
 driver.find_element_by_css_selector("[alt='Cart']").click()
 
 driver.find_element_by_xpath("//button[text()='PROCEED TO CHECKOUT']").click()
-wait=WebDriverWait(driver,10)
-wait.until(expected_conditions.presence_of_element_located((By.CLASS_NAME,"promoCode")))
-
-driver.find_element_by_css_selector("[placeholder='Enter promo code']").send_keys('rahulshettyacademy')
+#wait=WebDriverWait(driver,10)
+wait.until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR,"[placeholder='Enter promo code']")))
+driver.find_element_by_css_selector("[class='promoCode']").send_keys('rahulshettyacademy')
 driver.find_element_by_css_selector("[class='promoBtn']").click()
-wait.until(expected_conditions.presence_of_element_located((By.CLASS_NAME,"promoInfo")))
+wait.until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR,"[class='promoInfo']")))
 print(driver.find_element_by_css_selector("[class='promoInfo']").text)
 
