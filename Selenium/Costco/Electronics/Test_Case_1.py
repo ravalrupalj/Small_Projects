f35b14328821d32a6e9a0b1faf1ee022c27449b6
ravalrupalj <63676082+ravalrupalj@users.noreply.github.com>
2020-08-11 21:20:26 -0400

    Amazon Script

--- Selenium/Costco/Electronics/Test_Case_1.py
+++ Selenium/Costco/Electronics/Test_Case_1.py
@@ -1,16 +1,20 @@
 import time
 
 from selenium import webdriver
-#Test Case 1
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.support.wait import WebDriverWait
+
 
 driver= webdriver.Chrome(executable_path='C:\\Users\\raval\\Documents\\chromedriver_win32\\chromedriver.exe')
-driver.get('https://www.costco.ca/')
+driver.get('https://www.amazon.ca/')
+driver.implicitly_wait(5)
 driver.maximize_window()
-
-
-driver.find_element_by_xpath("//input[@value='ON']").click()
-driver.find_element_by_id("language-region-set").click()
-
+electronics=driver.find_element_by_id("nav-hamburger-menu")
+electronics.click()
+driver.find_element_by_link_text("Electronics, Computers & Office").click()
+driver.find_element_by_link_text("TV & Home Theatre").click()
+driver.find_element_by_link_text("Smart TVs").click()
+l=driver.find_elements_by_xpath("//div[@class='a-section a-spacing-none']/h2/a/span")
+for each in l:
+   print(each.text)
+   assert 'Smart' in each.text
+   assert 'TV' in each.text
+driver.find_element_by_xpath("//div[@id='brandsRefinements']/ul/li[1]/span").click()
