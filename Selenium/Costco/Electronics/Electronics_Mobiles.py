13012028f57f85f7b0f9b7d7ae0675902ab86ba0
ravalrupalj <63676082+ravalrupalj@users.noreply.github.com>
2020-08-23 22:02:48 -0400

    Electronics_Mobiles

---
+++ Selenium/Costco/Electronics/Electronics_Mobiles.py
@@ -0,0 +1,8 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.support.select import Select
+
+driver= webdriver.Chrome(executable_path='C:\\Users\\raval\\Documents\\chromedriver_win32\\chromedriver.exe')
+driver.get('https://www.amazon.ca/')
+driver.implicitly_wait(8)
+driver.maximize_window()
