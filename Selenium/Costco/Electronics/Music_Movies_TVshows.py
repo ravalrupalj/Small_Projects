c43c258853425811b7341e6aad367b71d116915d
ravalrupalj <63676082+ravalrupalj@users.noreply.github.com>
2020-09-22 21:59:59 -0400

    Continue_Music_Movies_Tvshows

--- Selenium/Costco/Electronics/Music_Movies_TVshows.py
+++ Selenium/Costco/Electronics/Music_Movies_TVshows.py
@@ -16,18 +16,24 @@ assert driver.find_element_by_xpath("//h1[contains(text(),'Movies & TV Series')]
 #Test case 3
 driver.find_element_by_xpath("//a[contains(text(),'Comedy')]").click()
 assert driver.find_element_by_xpath("//b[contains(text(),'Comedy')]").text =="Comedy"
-#Test case 3
+#Test case 4
 driver.find_element_by_xpath("//span[@class='a-size-small a-color-base'][contains(text(),'Movies')]").click()
 assert driver.find_element_by_xpath("//span[@class='a-color-state a-text-bold']").text=="Movies"
 Movies_List=driver.find_elements_by_xpath("//span[@class='a-size-medium a-color-base a-text-normal']")
 for each in Movies_List:
     print(each.text)
 driver.back()
+#Test case 5
 driver.find_element_by_xpath("//span[@class='a-size-small a-color-base'][contains(text(),'TV')]").click()
 assert driver.find_element_by_xpath("//span[@class='a-color-state a-text-bold']").text=="TV"
 TV_List=driver.find_elements_by_xpath("//span[@class='a-size-medium a-color-base a-text-normal']")
 for each in TV_List:
     print(each.text)
 driver.back()
+#Test case 6
 driver.find_element_by_xpath("//span[contains(text(),'Drama')]").click()
-assert driver.find_element_by_xpath("//span[@class='a-color-state a-text-bold']")=="Drama"
+assert driver.find_element_by_xpath("//span[@class='a-color-state a-text-bold']").text=="Drama"
+
+driver.back()
+#Test case 7
+assert driver.find_element_by_xpath("//span[@class='a-color-state a-text-bold'][contains(text(),'Exercise & Fitness')]").text=="Exercise & Fitness"
